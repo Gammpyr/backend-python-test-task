15699fcc3dd5517044c7fe747705eaf627bbf4f7
Gamid Abdulkhalikov <malak93@yandex.ru>
2026-05-10 12:39:37 +0300

    update urls

--- config/urls.py
+++ config/urls.py
@@ -17,14 +17,15 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    authentication_classes=[],
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+                  path('admin/', admin.site.urls),
 
-    path('', include('shop.urls', namespace='shop')),
-    path('users/', include('users.urls', namespace='users')),
+                  path('', include('shop.urls', namespace='shop')),
+                  path('users/', include('users.urls', namespace='users')),
 
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
